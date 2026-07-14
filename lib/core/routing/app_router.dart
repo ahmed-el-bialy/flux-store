afa0e5358cb83e73962f870c34e10492241bacb7
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-14 17:38:56 +0300

    feat: integrate GetAllProductsCubit into AppRouter for home screen navigation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flux_store/core/networking/dio_factory.dart';
+import 'package:flux_store/features/home/data/repo/home_repo.dart';
+import 'package:flux_store/features/home/data/web_services/web_services.dart';
 
+import '../../features/home/logic/get_all_products_cubit.dart';
 import '../../features/home/ui/home_screen.dart';
 import '../constants/app_constants.dart';
 
@@ -7,7 +12,13 @@ class AppRouter {
   Route generateRoute(RouteSettings setting) {
     switch (setting.name) {
       case AppConstants.homeScreen:
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
+        return MaterialPageRoute(builder: (_) =>
+            BlocProvider(
+              create: (context) =>
+                  GetAllProductsCubit(
+                      homeRepo: HomeRepo(WebServices(DioFactory.getDio())))..getAllProducts(),
+              child: HomeScreen(),
+            ));
 
       default:
         return MaterialPageRoute(builder: (_) => const HomeScreen());
