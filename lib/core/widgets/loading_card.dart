91b87b7f396594616a93958b981fa349791a7fa5
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-15 12:36:55 +0300

    feat: add LoadingCard and ProductsGridView widgets for loading state

---
+++ lib/core/widgets/loading_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flux_store/core/theming/app_colors.dart';
+import 'package:shimmer/shimmer.dart';
+
+class LoadingCard extends StatelessWidget {
+  const LoadingCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: AppColors.shimmerBase,
+      highlightColor: AppColors.shimmerHighlight,
+      period: const Duration(milliseconds: 1500),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 12.h),
+        child: Container(
+          width: 170.w,
+          height: 280.h,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(18.r),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/core/widgets/products_grid_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../features/home/data/models/product_model.dart';
+import 'product_card.dart';
+
+class ProductsGridView extends StatelessWidget {
+  const ProductsGridView({super.key, required this.models});
+
+  final List<ProductModel> models;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 0.6,
+        crossAxisCount: 2,
+      ),
+      itemCount: models.length,
+      itemBuilder: (context, index) {
+        return ProductCard(model: models[index]);
+      },
+    );
+  }
+}
