884301d5c02521cdca59a3c7699c2d85270aba0f
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-15 18:15:38 +0300

    feat: add sticky bottom CTA and refactor state-based UI in DetailsScreen

--- lib/features/details/ui/details_screen.dart
+++ lib/features/details/ui/details_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flux_store/features/details/logic/product_details_cubit.dart';
 import 'package:flux_store/features/details/ui/widgets/screen_body.dart';
-
+import 'package:flux_store/features/details/ui/widgets/sticky_bottom_cta.dart';
 import '../../../core/theming/app_colors.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -10,34 +10,43 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      backgroundColor: AppColors.white,
-      body: BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
-        builder: (context, state) {
-          if (state is ProductDetailsLoading) {
-            return Center(
+    return BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
+      builder: (context, state) {
+        if (state is ProductDetailsLoading) {
+          return Scaffold(
+            backgroundColor: AppColors.white,
+            body: Center(
               child: CircularProgressIndicator(color: AppColors.blue),
-            );
-          }
+            ),
+          );
+        }
 
-          if (state is ProductDetailsLoadedSuccessfully) {
-            final product = state.product;
-            return ScreenBody(model: state.product,);
-          }
+        if (state is ProductDetailsLoadedSuccessfully) {
+          final stockCount = state.product.stock ?? 0;
+          return Scaffold(
+            extendBody: true,
+            backgroundColor: AppColors.white,
+            body: ScreenBody(product: state.product),
+            bottomNavigationBar: StickyBottomCTA(stock: stockCount),
+          );
+        }
 
-          if (state is ProductDetailsFailed) {
-            return Center(
+        if (state is ProductDetailsFailed) {
+          return Scaffold(
+            backgroundColor: AppColors.white,
+            body: Center(
               child: Text(
                 state.errorMessage,
                 style: const TextStyle(color: Colors.red),
               ),
-            );
-          }
+            ),
+          );
+        }
 
-          return const Center(child: Text('there was an error...'));
-        },
-      ),
+        return const Scaffold(
+          body: Center(child: Text('there was an error...')),
+        );
+      },
     );
   }
 }
