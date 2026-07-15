413caca0a1f92db74206c68f126df595a2119bd4
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-15 16:15:08 +0300

    refactor: change AppColors to use const constants

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -1,10 +1,10 @@
 import 'dart:ui';
 
 class AppColors {
-  static final Color blue = Color(0xFF004AC6);
-  static final Color black = Color(0xFF16191C);
-  static final Color orange = Color(0xFFFEA619);
-  static final Color white = Color(0xFFF8F9FF);
+  static const Color blue = Color(0xFF004AC6);
+  static const Color black = Color(0xFF16191C);
+  static const Color orange = Color(0xFFFEA619);
+  static const Color white = Color(0xFFF8F9FF);
   static const Color shimmerBase = Color(0xFFE0E0E0);
   static const Color shimmerHighlight = Color(0xFFF5F5F5);
   static const Color lightBlue = Color(0xFFDBE5FA);
