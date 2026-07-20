3d116c8365788239ecadd0c1fb2b876f0dc305bc
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-20 15:29:21 +0300

    chore: reorganize asset structure and update category image paths

--- lib/features/categories/data/local/categories_data.dart
+++ lib/features/categories/data/local/categories_data.dart
@@ -1,47 +1,108 @@
+import 'package:flux_store/core/constants/app_constants.dart';
 import 'package:flux_store/features/categories/data/models/off_line_category_model.dart';
+
 class CategoriesData {
+  static final List<OffLineCategoryModel> categoryList = [
+    OffLineCategoryModel(
+      name: "Laptops",
+      image: "${AppConstants.categoryPath}/laptops.png",
+    ),
+    OffLineCategoryModel(
+      name: "Beauty",
+      image: "${AppConstants.categoryPath}/beauty.png",
+    ),
+    OffLineCategoryModel(
+      name: "Furniture",
+      image: "${AppConstants.categoryPath}/furniture.png",
+    ),
+    OffLineCategoryModel(
+      name: "Phones",
+      image: "${AppConstants.categoryPath}/phone.png",
+    ),
+    OffLineCategoryModel(
+      name: "sports-accessories",
+      image: "${AppConstants.categoryPath}/sports.png",
+    ),
+    OffLineCategoryModel(
+      name: "womens-jewellery",
+      image: "${AppConstants.categoryPath}/womens.png",
+    ),
+    OffLineCategoryModel(
+      name: "mens-watches",
+      image: "${AppConstants.categoryPath}/watch.png",
+    ),
 
-static final List<OffLineCategoryModel> categoryList = [
-  OffLineCategoryModel(name: "Laptops", image: "assets/laptops.png"),
-  OffLineCategoryModel(name: "Beauty", image: "assets/beauty.png"),
-  OffLineCategoryModel(name: "Furniture", image: "assets/furniture.png"),
-  OffLineCategoryModel(name: "Phones", image: "assets/phone.png"),
-  OffLineCategoryModel(name: "sports-accessories", image: "assets/sports.png"),
-  OffLineCategoryModel(name: "womens-jewellery", image: "assets/womens.png"),
-  OffLineCategoryModel(name: "mens-watches", image: "assets/watch.png"),
-
-  OffLineCategoryModel(name: "fragrances", image: "assets/fragrances.png"),
-  OffLineCategoryModel(name: "groceries", image: "assets/groceries.png"),
-  OffLineCategoryModel(
-    name: "home-decoration",
-    image: "assets/home_decoration.png",
-  ),
-  OffLineCategoryModel(
-    name: "kitchen-accessories",
-    image: "assets/kitchen.png",
-  ),
-  OffLineCategoryModel(name: "mens-shirts", image: "assets/mens_shirts.png"),
-  OffLineCategoryModel(name: "mens-shoes", image: "assets/mens_shoes.png"),
-  OffLineCategoryModel(
-    name: "mobile-accessories",
-    image: "assets/mobile_accessories.png",
-  ),
-  OffLineCategoryModel(name: "motorcycle", image: "assets/motorcycle.png"),
-  OffLineCategoryModel(name: "skin-care", image: "assets/skin_care.png"),
-  OffLineCategoryModel(name: "smartphones", image: "assets/smartphones.png"),
-  OffLineCategoryModel(name: "sunglasses", image: "assets/sunglasses.png"),
-  OffLineCategoryModel(name: "tablets", image: "assets/tablets.png"),
-  OffLineCategoryModel(name: "tops", image: "assets/tops.png"),
-  OffLineCategoryModel(name: "vehicle", image: "assets/vehicle.png"),
-  OffLineCategoryModel(name: "womens-bags", image: "assets/womens_bags.png"),
-  OffLineCategoryModel(
-    name: "womens-dresses",
-    image: "assets/womens_dresses.png",
-  ),
-  OffLineCategoryModel(name: "womens-shoes", image: "assets/womens_shoes.png"),
-  OffLineCategoryModel(
-    name: "womens-watches",
-    image: "assets/womens_watches.png",
-  ),
-];
+    OffLineCategoryModel(
+      name: "fragrances",
+      image: "${AppConstants.categoryPath}/fragrances.png",
+    ),
+    OffLineCategoryModel(
+      name: "groceries",
+      image: "${AppConstants.categoryPath}/groceries.png",
+    ),
+    OffLineCategoryModel(
+      name: "home-decoration",
+      image: "${AppConstants.categoryPath}/home_decoration.png",
+    ),
+    OffLineCategoryModel(
+      name: "kitchen-accessories",
+      image: "${AppConstants.categoryPath}/kitchen.png",
+    ),
+    OffLineCategoryModel(
+      name: "mens-shirts",
+      image: "${AppConstants.categoryPath}/mens_shirts.png",
+    ),
+    OffLineCategoryModel(
+      name: "mens-shoes",
+      image: "${AppConstants.categoryPath}/mens_shoes.png",
+    ),
+    OffLineCategoryModel(
+      name: "mobile-accessories",
+      image: "${AppConstants.categoryPath}/mobile_accessories.png",
+    ),
+    OffLineCategoryModel(
+      name: "motorcycle",
+      image: "${AppConstants.categoryPath}/motorcycle.png",
+    ),
+    OffLineCategoryModel(
+      name: "skin-care",
+      image: "${AppConstants.categoryPath}/skin_care.png",
+    ),
+    OffLineCategoryModel(
+      name: "smartphones",
+      image: "${AppConstants.categoryPath}/smartphones.png",
+    ),
+    OffLineCategoryModel(
+      name: "sunglasses",
+      image: "${AppConstants.categoryPath}/sunglasses.png",
+    ),
+    OffLineCategoryModel(
+      name: "tablets",
+      image: "${AppConstants.categoryPath}/tablets.png",
+    ),
+    OffLineCategoryModel(
+      name: "tops",
+      image: "${AppConstants.categoryPath}/tops.png",
+    ),
+    OffLineCategoryModel(
+      name: "vehicle",
+      image: "${AppConstants.categoryPath}/vehicle.png",
+    ),
+    OffLineCategoryModel(
+      name: "womens-bags",
+      image: "${AppConstants.categoryPath}/womens_bags.png",
+    ),
+    OffLineCategoryModel(
+      name: "womens-dresses",
+      image: "${AppConstants.categoryPath}/womens_dresses.png",
+    ),
+    OffLineCategoryModel(
+      name: "womens-shoes",
+      image: "${AppConstants.categoryPath}/womens_shoes.png",
+    ),
+    OffLineCategoryModel(
+      name: "womens-watches",
+      image: "${AppConstants.categoryPath}/womens_watches.png",
+    ),
+  ];
 }
