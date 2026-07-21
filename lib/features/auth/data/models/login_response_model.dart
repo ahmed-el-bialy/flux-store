5e64d5f227527b774c8added1d1c4926b570cfac
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-07-21 14:02:19 +0300

    feat: add shared_preferences dependency

---
+++ lib/features/auth/data/models/login_response_model.dart
@@ -0,0 +1,31 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'login_response_model.g.dart';
+
+@JsonSerializable()
+class LoginResponseModel {
+  final int id;
+  final String username;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String image;
+  final String token;
+
+  LoginResponseModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+    required this.token,
+  });
+
+  factory LoginResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$LoginResponseModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LoginResponseModelToJson(this);
+}
